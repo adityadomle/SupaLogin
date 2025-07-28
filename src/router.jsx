82332123e2a5d4f400fde78a5409f1406e7380f3
Aditya Domle <adityadomle14@gmail.com>
2025-07-28 21:03:07 +0530

    solve the issue

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Signup from "./components/Signup";
-import Signin from "./components/Signin";
-import Dashboard from "./routes/Dashboard";
-import PrivateRoute from "./components/PrivateRoute";
+import Signup from "./components/Signup.jsx";
+import Signin from "./components/Signin.jsx";
+import Dashboard from "./routes/Dashboard.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
